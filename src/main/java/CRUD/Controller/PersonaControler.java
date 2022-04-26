1238db9b927947fc6b8ca0b5ab54a1c02d5d206f
Alvaro297 <alvaromartinfalagan29@gmail.com>
2022-04-26 12:46:01 +0200

    Ejercicio CRUD + BS8

--- src/main/java/CRUD/Controller/PersonaControler.java
+++ src/main/java/CRUD/Controller/PersonaControler.java
@@ -1,10 +1,12 @@
 package CRUD.Controller;
 
+import CRUD.Controller.Errores.ErrorBean.BeanNotFoundException;
 import CRUD.Controller.Person.Persona;
 import CRUD.Controller.dto.PersonaInputDTO;
 import CRUD.Controller.dto.PersonaOutputDTO;
 import CRUD.Controller.servicios.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,28 +22,48 @@ public class PersonaControler {
 
     @PostMapping("/addPersona")
     public String addPersona(@RequestBody PersonaInputDTO persona) throws Exception{
+        try{
         personaService.addPersona(persona);
+        }catch (Exception e){
+            throw new BeanNotFoundException(e.getMessage());
+        }
         return "Persona creada correctamente";
     }
 
     @GetMapping("/getPerson/{id}")
     public Persona getPersonById(@PathVariable Integer id) throws Exception {
-        return personaRepository.findById(id).orElseThrow(()->new Exception("No encontrado"));
+        try{
+            return personaRepository.findById(id).orElseThrow(()->new Exception("No encontrado"));
+        }catch (Exception e){
+            throw new BeanNotFoundException(e.getMessage());
+        }
     }
 
     @GetMapping("/getPersona")
     public List<Persona> getPersona(){
-        return personaRepository.findAll();
+        try{
+            return personaRepository.findAll();
+        }catch (Exception e){
+             throw new BeanNotFoundException(e.getMessage());
+        }
     }
 
     @GetMapping("/getPerson/name/{name}")
     public List<PersonaOutputDTO> deletePersonaByName(@PathVariable String name) {
-        return personaRepository.findByName(name);
+        try{
+            return personaRepository.findByName(name);
+         }catch (Exception e){
+             throw new BeanNotFoundException(e.getMessage());
+         }
     }
 
     @DeleteMapping("/delete/{id}")
     public String deletePersonaById(@PathVariable Integer id) throws Exception {
+        try{
         personaService.deletedById(id);
+        }catch (Exception e){
+            throw new BeanNotFoundException(e.getMessage());
+        }
         return "El id "+id+" ha sido borrado";
 
     }
